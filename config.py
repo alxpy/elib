75c21942d95210c5bd592664a3d0bf32c5a706ad
alex <kuzmenko_s@ukr.net>
2014-05-17 16:48:25 +0300

    fixed config

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
 CSRF_ENABLED = True
 SECRET_KEY = 'JHGJH45#%@#)h#ghGJHGJhg#jhg@$hjG'
+
+try:
+    DATABASE_URL = os.environ['DATABASE_URL']
+except:
+    DATABASE_URL = 'sqlite:///sqlite.db'
+
 # RECAPTCHA_PUBLIC_KEY = '#############'
--- elib/database.py
+++ elib/database.py
@@ -4,10 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-try:
-    DATABASE_URL = os.environ['DATABASE_URL']
-except:
-    DATABASE_URL = 'sqlite:///sqlite.db'
+from config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL, convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
